4d8dc1c2f0f8a49322008c2677e209fc7e3ab204
GleidsonSousa <gleidson2090@gmail.com>
2022-09-19 11:15:14 -0300

    19/09

--- estacionamento/back/src/script.sql
+++ estacionamento/back/src/script.sql
@@ -17,31 +17,36 @@ create table carros(
     id_cli integer not null,
     placa varchar(10) not null unique,
     marca varchar(50) not null,
-    tipo varchar(20) not null,
+    tipo varchar(30) not null,
     descrição varchar(100)
 );
 
 create table vagas(
     id_vaga integer auto_increment not null primary key,
-    tipo_vaga varchar(20) not null
+    tipo_vaga varchar(30) not null
 
 );
+
 create table registraTempo(
     id_registro integer auto_increment not null primary key,
     id_vaga integer not null,
-    id_carro integer not null,
+    placa_carro integer not null,
     id_cliente integer not null, 
     data date not null,
     h_entrada time not null,
     h_saida time,
-    tipo_reg varchar(20) not null,
+    tipo_reg varchar(30) not null,
     valor_h float(6,2) not null,
+    forma_pagamento varchar(30) not null,
+    status_pag varchar(10) not null,
     foreign key (id_vaga) references vagas(id_vaga),
     foreign key (id_cliente) references clientes(id_cliente),
-    foreign key (id_carro) references carros(id_carro)
+    foreign key (placa_carro) references carros(placa)
 );
 
 alter table carros add foreign key (id_cli) references clientes(id_cliente);
+
+
 
 LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/estacionamento/back/src/docs/clientes.csv'
 INTO TABLE clientes
@@ -67,6 +72,13 @@ IGNORE 1 ROWS;
 select * from `clientes`;
 select * from `carros`;
 select * from `vagas`;
+
+create view vw_estacionar as
+select 
+inner join locacoes l on c.codigo = l.codigo_cli
+inner join filmes f on l.codigo_filme = f.codigo where l.data_devolucao is null;
+
+select * from vw_devolucoes;
 
 
 
