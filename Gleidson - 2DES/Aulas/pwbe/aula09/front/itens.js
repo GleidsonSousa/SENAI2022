844a43c69144745fc0febdf3d90516ba80217a96
GleidsonSousa <gleidson2090@gmail.com>
2022-10-04 11:19:07 -0300

    04/10

---
+++ Gleidson - 2DES/Aulas/pwbe/aula09/front/itens.js
@@ -0,0 +1,48 @@
+const uri='http://localhost:3000/patrimonio/itens/';
+ 
+var itens = [];
+
+function carregar(){
+    const options = {method: 'GET'};
+
+fetch(uri, options)
+  .then(res => res.json())
+  .then(res => {
+    itens = res;
+    preencherTela();
+    })
+  .catch(err => console.error(err));
+
+}
+
+const corpo = document.querySelector('#corpo');
+
+function preencherTela(){
+    itens.forEach(e =>{
+      let card = document.querySelector(".card").cloneNode(true);
+      card.classList.remove("model");
+      card.querySelector("#ni").innerHTML += e.ni;
+      card.querySelector("#aquisicao").innerHTML += e.aquisicao;
+      card.querySelector("#denominacao").innerHTML += e.denominacao;
+      card.querySelector("#valor").innerHTML += e.valor;
+      card.querySelector("#img").src = '../docs/assets/' + e.img;
+      card.querySelector("#del").setAttribute('onclick', `del(${e.ni})`);
+      corpo.appendChild(card);
+    })
+}
+
+const del = (ni) => {
+    const options = {
+        method: 'DELETE',
+    };
+    if( confirm("Confirma a exclusão do item" + ni)){
+        fetch(uri + ni, options)
+        .then(res => res.status)
+        .then(res  => {
+            window.location.reload();
+                
+                
+        })
+        .catch(err => console.error(err));
+    }
+}
