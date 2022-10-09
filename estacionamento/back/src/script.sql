62f8e56c6cbc2e180cfccf57adafdfa785259ead
Gleisin <gleidson2090@gmail.com>
2022-10-09 14:14:36 -0300

    new att

--- estacionamento/back/src/script.sql
+++ estacionamento/back/src/script.sql
@@ -40,7 +40,7 @@ create table carros(
 );
 
 create table vagas(
-    id_vaga integer auto_increment not null primary key,
+    id_vaga numeric(10) not null primary key,
     categoria_vaga varchar(30) not null,
     valor_h float(6,2) not null
 
@@ -49,7 +49,7 @@ create table vagas(
 
 create table registro_estac(
     id_registro integer auto_increment not null primary key,
-    id_vag integer not null,
+    id_vag numeric(10) not null,
     id_car integer not null,
     id_cli integer not null, 
     data date not null,
@@ -77,129 +77,168 @@ insert into telefones values(1,'Fixo','(33)6604-1210');
 insert into telefones values(2,'Celular','(49)93736-5429');
 insert into telefones values(3,'Celular','(19)93838-2238');
 
-
-
 insert into carros values(default,1,'MWK7015','Scania','Caminhão');
 insert into carros values(default,3,'EDL3Z90','Ferrari','Carro');
 insert into carros values(default,2,'CIZ8920','Honda','Moto');
 insert into carros values(default,3,'HXW3364','BMW','Carro');
 
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
-insert into vagas values(default,'Veículo Pequeno',5.00);
-insert into vagas values(default,'Veículo Médio',10.00);
-insert into vagas values(default,'Veículo Grande',20.00);
+insert into vagas values(1,'Veículo Pequeno',5.00);
+insert into vagas values(2,'Veículo Pequeno',5.00);
+insert into vagas values(3,'Veículo Pequeno',5.00);
+insert into vagas values(4,'Veículo Pequeno',5.00);
+insert into vagas values(5,'Veículo Pequeno',5.00);
+insert into vagas values(6,'Veículo Pequeno',5.00);
+insert into vagas values(7,'Veículo Pequeno',5.00);
+insert into vagas values(8,'Veículo Pequeno',5.00);
+insert into vagas values(9,'Veículo Pequeno',5.00);
+insert into vagas values(10,'Veículo Pequeno',5.00);
+insert into vagas values(11,'Veículo Pequeno',5.00);
+insert into vagas values(12,'Veículo Pequeno',5.00);
+insert into vagas values(13,'Veículo Pequeno',5.00);
+insert into vagas values(14,'Veículo Pequeno',5.00);
+insert into vagas values(15,'Veículo Pequeno',5.00);
+insert into vagas values(16,'Veículo Pequeno',5.00);
+insert into vagas values(17,'Veículo Pequeno',5.00);
+insert into vagas values(18,'Veículo Pequeno',5.00);
+insert into vagas values(19,'Veículo Pequeno',5.00);
+insert into vagas values(20,'Veículo Pequeno',5.00);
+insert into vagas values(21,'Veículo Pequeno',5.00);
+insert into vagas values(22,'Veículo Pequeno',5.00);
+insert into vagas values(23,'Veículo Pequeno',5.00);
+insert into vagas values(24,'Veículo Pequeno',5.00);
+insert into vagas values(25,'Veículo Pequeno',5.00);
+insert into vagas values(26,'Veículo Pequeno',5.00);
+insert into vagas values(27,'Veículo Pequeno',5.00);
+insert into vagas values(28,'Veículo Pequeno',5.00);
+insert into vagas values(29,'Veículo Pequeno',5.00);
+insert into vagas values(30,'Veículo Pequeno',5.00);
+insert into vagas values(31,'Veículo Pequeno',5.00);
+insert into vagas values(32,'Veículo Pequeno',5.00);
+insert into vagas values(33,'Veículo Pequeno',5.00);
+insert into vagas values(34,'Veículo Pequeno',5.00);
+insert into vagas values(35,'Veículo Pequeno',5.00);
+insert into vagas values(36,'Veículo Pequeno',5.00);
+insert into vagas values(37,'Veículo Pequeno',5.00);
+insert into vagas values(38,'Veículo Pequeno',5.00);
+insert into vagas values(39,'Veículo Pequeno',5.00);
+insert into vagas values(40,'Veículo Pequeno',5.00);
+insert into vagas values(41,'Veículo Pequeno',5.00);
+insert into vagas values(42,'Veículo Pequeno',5.00);
+insert into vagas values(43,'Veículo Pequeno',5.00);
+insert into vagas values(44,'Veículo Pequeno',5.00);
+insert into vagas values(45,'Veículo Pequeno',5.00);
+insert into vagas values(46,'Veículo Pequeno',5.00);
+insert into vagas values(47,'Veículo Pequeno',5.00);
+insert into vagas values(48,'Veículo Pequeno',5.00);
+insert into vagas values(49,'Veículo Pequeno',5.00);
+insert into vagas values(50,'Veículo Pequeno',5.00);
+
+insert into vagas values(1,'Veículo Médio',10.00);
+insert into vagas values(2,'Veículo Médio',10.00);
+insert into vagas values(3,'Veículo Médio',10.00);
+insert into vagas values(4,'Veículo Médio',10.00);
+insert into vagas values(5,'Veículo Médio',10.00);
+insert into vagas values(6,'Veículo Médio',10.00);
+insert into vagas values(7,'Veículo Médio',10.00);
+insert into vagas values(8,'Veículo Médio',10.00);
+insert into vagas values(9,'Veículo Médio',10.00);
+insert into vagas values(10,'Veículo Médio',10.00);
+insert into vagas values(11,'Veículo Médio',10.00);
+insert into vagas values(12,'Veículo Médio',10.00);
+insert into vagas values(13,'Veículo Médio',10.00);
+insert into vagas values(14,'Veículo Médio',10.00);
+insert into vagas values(15,'Veículo Médio',10.00);
+insert into vagas values(16,'Veículo Médio',10.00);
+insert into vagas values(17,'Veículo Médio',10.00);
+insert into vagas values(18,'Veículo Médio',10.00);
+insert into vagas values(19,'Veículo Médio',10.00);
+insert into vagas values(20,'Veículo Médio',10.00);
+insert into vagas values(21,'Veículo Médio',10.00);
+insert into vagas values(22,'Veículo Médio',10.00);
+insert into vagas values(23,'Veículo Médio',10.00);
+insert into vagas values(24,'Veículo Médio',10.00);
+insert into vagas values(25,'Veículo Médio',10.00);
+insert into vagas values(26,'Veículo Médio',10.00);
+insert into vagas values(27,'Veículo Médio',10.00);
+insert into vagas values(28,'Veículo Médio',10.00);
+insert into vagas values(29,'Veículo Médio',10.00);
+insert into vagas values(30,'Veículo Médio',10.00);
+insert into vagas values(31,'Veículo Médio',10.00);
+insert into vagas values(32,'Veículo Médio',10.00);
+insert into vagas values(33,'Veículo Médio',10.00);
+insert into vagas values(34,'Veículo Médio',10.00);
+insert into vagas values(35,'Veículo Médio',10.00);
+insert into vagas values(36,'Veículo Médio',10.00);
+insert into vagas values(37,'Veículo Médio',10.00);
+insert into vagas values(38,'Veículo Médio',10.00);
+insert into vagas values(39,'Veículo Médio',10.00);
+insert into vagas values(40,'Veículo Médio',10.00);
+insert into vagas values(41,'Veículo Médio',10.00);
+insert into vagas values(42,'Veículo Médio',10.00);
+insert into vagas values(43,'Veículo Médio',10.00);
+insert into vagas values(44,'Veículo Médio',10.00);
+insert into vagas values(45,'Veículo Médio',10.00);
+insert into vagas values(46,'Veículo Médio',10.00);
+insert into vagas values(47,'Veículo Médio',10.00);
+insert into vagas values(48,'Veículo Médio',10.00);
+insert into vagas values(49,'Veículo Médio',10.00);
+insert into vagas values(50,'Veículo Médio',10.00);
+
+insert into vagas values(1,'Veículo Grande',20.00);
+insert into vagas values(2,'Veículo Grande',20.00);
+insert into vagas values(3,'Veículo Grande',20.00);
+insert into vagas values(4,'Veículo Grande',20.00);
+insert into vagas values(5,'Veículo Grande',20.00);
+insert into vagas values(6,'Veículo Grande',20.00);
+insert into vagas values(7,'Veículo Grande',20.00);
+insert into vagas values(8,'Veículo Grande',20.00);
+insert into vagas values(9,'Veículo Grande',20.00);
+insert into vagas values(10,'Veículo Grande',20.00);
+insert into vagas values(11,'Veículo Grande',20.00);
+insert into vagas values(12,'Veículo Grande',20.00);
+insert into vagas values(13,'Veículo Grande',20.00);
+insert into vagas values(14,'Veículo Grande',20.00);
+insert into vagas values(15,'Veículo Grande',20.00);
+insert into vagas values(16,'Veículo Grande',20.00);
+insert into vagas values(17,'Veículo Grande',20.00);
+insert into vagas values(18,'Veículo Grande',20.00);
+insert into vagas values(19,'Veículo Grande',20.00);
+insert into vagas values(20,'Veículo Grande',20.00);
+insert into vagas values(21,'Veículo Grande',20.00);
+insert into vagas values(22,'Veículo Grande',20.00);
+insert into vagas values(23,'Veículo Grande',20.00);
+insert into vagas values(24,'Veículo Grande',20.00);
+insert into vagas values(25,'Veículo Grande',20.00);
+insert into vagas values(26,'Veículo Grande',20.00);
+insert into vagas values(27,'Veículo Grande',20.00);
+insert into vagas values(28,'Veículo Grande',20.00);
+insert into vagas values(29,'Veículo Grande',20.00);
+insert into vagas values(30,'Veículo Grande',20.00);
+insert into vagas values(31,'Veículo Grande',20.00);
+insert into vagas values(32,'Veículo Grande',20.00);
+insert into vagas values(33,'Veículo Grande',20.00);
+insert into vagas values(34,'Veículo Grande',20.00);
+insert into vagas values(35,'Veículo Grande',20.00);
+insert into vagas values(36,'Veículo Grande',20.00);
+insert into vagas values(37,'Veículo Grande',20.00);
+insert into vagas values(38,'Veículo Grande',20.00);
+insert into vagas values(39,'Veículo Grande',20.00);
+insert into vagas values(40,'Veículo Grande',20.00);
+insert into vagas values(41,'Veículo Grande',20.00);
+insert into vagas values(42,'Veículo Grande',20.00);
+insert into vagas values(43,'Veículo Grande',20.00);
+insert into vagas values(44,'Veículo Grande',20.00);
+insert into vagas values(45,'Veículo Grande',20.00);
+insert into vagas values(46,'Veículo Grande',20.00);
+insert into vagas values(47,'Veículo Grande',20.00);
+insert into vagas values(48,'Veículo Grande',20.00);
+insert into vagas values(49,'Veículo Grande',20.00);
+insert into vagas values(50,'Veículo Grande',20.00);
 
 insert into registro_estac values(default,1,3,2,DATE_SUB(curdate(),INTERVAL 5 DAY),'08:00','','','','Aberto');
 insert into registro_estac values(default,3,1,1,DATE_SUB(curdate(),INTERVAL 5 DAY),'09:30','','','','Aberto');
 insert into registro_estac values(default,2,2,3,DATE_SUB(curdate(),INTERVAL 2 DAY),'10:00','','','','Aberto');
 insert into registro_estac values(default,2,4,3,DATE_SUB(curdate(),INTERVAL 6 DAY),'08:30','12:30',40.00,'Cartão Débito','Pago');
-
-select * from `clientes`;
-select * from `carros`;
-select * from `vagas`;
-select * from `registro_estac`;
-
 
 create view vw_clientes as
 select c.id_cliente as cliente_id, c.nome as Nome_cliente, c.cpf, c.email , c.status_cli,t.tipo_tel, t.numero as telefone from clientes c
@@ -222,6 +261,14 @@ select *, (valor_final) as v_final from registro_estac where h_saida  <> "";
 select * from estacionamento_pagos;
 
 
+select * from `clientes`;
+select * from `telefones`;
+select * from `carros`;
+select * from `vagas`;
+select * from `registro_estac`;
+select * from vw_clientes;
+select * from vw_estacionar;
+select * from estacionamento_pagos;
 
 
 
